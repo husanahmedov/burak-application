321402a535d0722e9e9e6d2b0264b26aaf4f25e8
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-10-11 23:26:38 +0900

    feat: develop member schema & service model

---
+++ src/lib/enum/member.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Enums
+ *  **/
+
+export enum memberType {
+  USER = "USER",
+  RESTAURANT = "RESTAURANT",
+}
+
+export enum memberStatus {
+  ACTIVE = "ACTIVE",
+  BLOCK = "BLOCK",
+  DELETE = "DELETE",
+}
--- src/restaurantRouter.ts
+++ src/restaurantRouter.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import restaurantController from "./controllers/restaurant.controller";
 const restaurantRouter = express.Router();
 
+import MemberService from './models/Member.service';
+
 restaurantRouter.get("/", restaurantController.goHome);
 
 restaurantRouter.get("/login", restaurantController.getLogin);
