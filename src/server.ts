5b1feb49edbfbc8ead4982d7e811c90ad30c9fe3
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-10-11 17:51:17 +0900

    feat: MITASK-H

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-
 import app from './app';
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
-    console.log("connection to database is succedd")
-    const PORT = process.env.PORT ?? 3003
+    console.log("Application is connected to database successfully🎆")
+    const PORT = process.env.PORT ?? 3000
     app.listen(PORT, function() {
-      console.log("DONE", PORT);
+      console.log(`Application is running on http://localhost:${PORT} 🚀`);
     })
   })
   .catch((error) => {
--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,31 @@
 /**
  * @param arr Number containing array
  * which a number needed to be found as a largest integer in the array
-*/
-const getHighestNumberIndex = (arr: number[]): number => {
-   let largestNumber: number = 0;
-   arr.forEach((element: number) => {
-    if(element > largestNumber) {
-        largestNumber = element
-    }
-   })
-   return arr.indexOf(largestNumber)
-}
+ */
+// const getHighestNumberIndex = (arr: number[]): number => {
+//    let largestNumber: number = 0;
+//    arr.forEach((element: number) => {
+//     if(element > largestNumber) {
+//         largestNumber = element
+//     }
+//    })
+//    return arr.indexOf(largestNumber)
+// }
 
-console.log(getHighestNumberIndex([1, 4, 30, 7, 60]));
+// console.log(getHighestNumberIndex([1, 4, 30, 7, 60]));
+
+/**
+ * @param arr Number containing array
+ * GetPositive()
+ * **/
+
+const getPositive = (param: number[]): string => {
+  let resultString: string = "";
+  const changedValue = param.filter((value) => value > 0);
+  changedValue.forEach((element: number) => {
+    resultString += element.toString();
+  });
+  return resultString;
+};
+
+getPositive([3, -7, -8, 9, 4]);
