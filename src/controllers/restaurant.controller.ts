451fc7d2ed693e2b388cc02a9bfdd99fb3a5beb2
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-10-21 23:53:07 +0900

    feat: develop admin frontend pages and integrate header & footer

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,56 +1,62 @@
-import {Request, Response} from "express";
-import {T} from "../lib/types/common";
+import { Request, Response } from 'express';
+import { T } from '../lib/types/common';
 
-import MemberService from "../models/Member.service";
-import {LoginInput, Member, MemberInput} from "../lib/member";
+import MemberService from '../models/Member.service';
+import { LoginInput, Member, MemberInput } from '../lib/member';
 
 const restaurantController: T = {};
 
 const memberService = new MemberService();
 
 restaurantController.goHome = (request: Request, response: Response) => {
-    try {
-        console.log("Main Page Loaded")
-        response.send("Main Page");
-    } catch (error) {
-        console.log("You have an error", error)
-    }
+  try {
+    console.log('Main Page Loaded');
+    response.render('home');
+  } catch (error) {
+    console.log('You have an error', error);
+  }
 };
 
 restaurantController.getLogin = (request: Request, response: Response) => {
-    try {
-        console.log("Login Page Loaded")
-        response.send("Login Page");
-    } catch (error) {
-        console.log("You have an error", error)
-    }
+  try {
+    console.log('Login Page Loaded');
+    response.render('login');
+  } catch (error) {
+    console.log('You have an error', error);
+  }
 };
 
-restaurantController.processSignup = async (request: Request, response: Response) => {
-    try {
-        console.log("Signup Process Loaded")
-        const newMember: MemberInput = request.body,
-            result: Member = await memberService.processSignup(newMember);
-        response.send(result);
-    } catch (error) {
-        response.send(error);
-    }
-}
+restaurantController.processSignup = async (
+  request: Request,
+  response: Response,
+) => {
+  try {
+    console.log('Signup Process Loaded');
+    const newMember: MemberInput = request.body,
+      result: Member = await memberService.processSignup(newMember);
+    response.send(result);
+  } catch (error) {
+    response.send(error);
+  }
+};
 
-restaurantController.processLogin = async (request: Request, response: Response) => {
-    try {
-        console.log("Login Process Loaded");
-        const input: LoginInput = request.body,
-            result = await memberService.processLogin(input);
-        response.send(result);
-    } catch (error) {
-        console.log("You have an error", error)
-        response.send(error);
-    }
-}
+restaurantController.processLogin = async (
+  request: Request,
+  response: Response,
+) => {
+  try {
+    console.log('Login Process Loaded');
+    const input: LoginInput = request.body,
+      result = await memberService.processLogin(input);
+    response.send(result);
+  } catch (error) {
+    console.log('You have an error', error);
+    response.send(error);
+  }
+};
 
 restaurantController.getSignup = (request: Request, response: Response) => {
-    response.send("Signup Page");
+  response.render('signup.ejs');
 };
 
 export default restaurantController;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import app from './app';
 
 dotenv.config();
 
-import mongoose from "mongoose";
-import app from './app';
-
 mongoose
-    .connect(process.env.MONGO_URL as string, {})
-    .then((data) => {
-        console.log("Application is connected to database successfully🎆")
-        const PORT = process.env.PORT ?? 3000
-        app.listen(PORT, function () {
-            console.log(`Application is running on http://localhost:${PORT} 🚀`);
-        })
-    })
-    .catch((error) => {
-        console.log("ERROR on connection to MongoDB", error);
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log('Application is connected to database successfully🎆');
+    const PORT = process.env.PORT ?? 3000;
+    app.listen(PORT, function () {
+      console.info(`Application is running on http://localhost:${PORT} 🚀`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
     });
+  })
+  .catch((error) => {
+    console.log('ERROR on connection to MongoDB', error);
+  });
