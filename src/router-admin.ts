6c9f6879ef80d8f0e8d06423fec9af49579dd66d
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-10-25 12:19:21 +0900

    feat: create product controller and product routing apis

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import restaurantController from './controllers/restaurant.controller';
+import productController from './controllers/product.controller';
 
 const routerAdmin = express.Router();
 
 routerAdmin.get('/', restaurantController.goHome);
 
+// RESTAURANT
 routerAdmin
   .get('/login', restaurantController.getLogin)
   .post('/login', restaurantController.processLogin);
@@ -15,5 +17,11 @@ routerAdmin
 
 routerAdmin.get('/logout', restaurantController.logout);
 routerAdmin.get('/check-me', restaurantController.checkAuthSession);
+
+// PRODUCTS
+routerAdmin
+  .get('/product/all', productController.getAllProducts)
+  .post('/product/create', productController.createNewProduct)
+  .post('/product/:id', productController.updateChosenProduct);
 
 export default routerAdmin;
