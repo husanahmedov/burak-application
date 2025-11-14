66c41ed766dfe3c4c224c159cdb01d4c64e257a3
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-11-14 17:50:55 +0900

    feat: MITASK-S

--- src/train.ts
+++ src/train.ts
@@ -82,10 +82,11 @@
 
 // console.log(calculateSumOfNumbers([4, 5, 'salom', { number: 10 }, 4]));
 
-function objectToArray<T extends Record<string, any>>(
-  param: T
-): [string, T[keyof T]][] {
-  return Object.entries(param);
+function missingNumberFunc(nums: number[]): number {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((acc, num) => acc + num, 0);
+  return expectedSum - actualSum;
 }
 
-console.log(objectToArray({ a: 10, b: 20 }));
+console.log(missingNumberFunc([3, 0, 1]));
