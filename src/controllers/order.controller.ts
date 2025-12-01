284816b7dec282a4026dcc447c99589a252d242a
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-12-01 17:18:27 +0900

    feat: develop create Order API

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,24 @@
+import { ExtendedRequest } from '../lib/member';
+import { T } from '../lib/types/common';
+import { Response } from 'express';
+import Errors, { HttpCode } from '../lib/Errors';
+import OrderService from '../models/Order.service';
+
+const orderController: T = {};
+const orderService = new OrderService();
+
+orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log('createOrder');
+
+    const result = await orderService.createOrder(req.member, req.body);
+
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log('Error:createOrder', err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+export default orderController;
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import memberController from './controllers/member.controller';
 import uploader from './lib/utils/uploader';
 import productController from './controllers/product.controller';
+import orderController from './controllers/order.controller';
 
 const router = express.Router();
 
@@ -31,5 +32,12 @@ router
     memberController.retrieveAuth,
     productController.getProduct
   );
+
+/** ORDER **/
+router.post(
+  '/order/create',
+  memberController.verifyAuth,
+  orderController.createOrder
+);
 
 export default router;
